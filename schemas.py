da9cd2e71d76577f22ab32bb8968e0e4319d1ade
vipmohsin <mkmohsin404@gmail.com>
2026-09-17 15:16:07 -0700

    Feat: added custom vaidations

--- schemas.py
+++ schemas.py
@@ -1,10 +1,20 @@
-from pydantic import BaseModel,Field
+from pydantic import BaseModel,Field, field_validator
 
 class StudentCreate(BaseModel):
-    name:str
+    name:str = Field(
+        min_length=3,
+        pattern=r"^[A-Za-z ]+$"
+    )
     age:int= Field(ge=5 , le=80)
     marks:float = Field(ge=0 , le=100)
     
+    @field_validator("name")
+    @classmethod
+    def validate_name(cls, value):
+        if len(value.replace(" ", "")) < 3:
+            raise ValueError("Name must contain at least 3 letters.")
+
+        return value
 
 class StudentResponse(BaseModel):
     roll_no: int
